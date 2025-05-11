a78af5e42578cc8302d33c54abf395efac9a70ca
Glody Mafo <glodymafokis@gmail.com>
2025-05-11 15:36:55 +0200

    Week01

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int answer = -1;
+
+
+        while (answer != magicNumber)
+        {
+            Console.Write("What is your guess?");
+            string guess = Console.ReadLine();
+            answer = int.Parse(guess);
+
+            if (answer > magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (answer < magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You Guessed It");
+            }
+        }
+
     }
 }
