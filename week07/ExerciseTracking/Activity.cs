9aae246d289f382bf20039b60f758c47c379cba4
Glody Mafo <glodymafokis@gmail.com>
2025-06-17 18:07:29 +0200

    week07

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,26 @@
+using System;
+
+public abstract class Activity
+{
+    private DateTime _date;
+    private int _lengthInMinutes;
+
+    public Activity(DateTime date, int lengthInMinutes)
+    {
+        _date = date;
+        _lengthInMinutes = lengthInMinutes;
+    }
+
+    public DateTime Date => _date;
+    public int LengthInMinutes => _lengthInMinutes;
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();  
+    public abstract double GetPace();  
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {this.GetType().Name} ({_lengthInMinutes} min): " +
+               $"Distance: {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.00} min per km";
+    }
+}
